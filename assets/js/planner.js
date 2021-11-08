82990ddeebef7cc2f32812d6dbb3da49a2add7d9
a.breezy <ambrose.wilkins@gmail.com>
2021-11-08 18:20:41 -0500

    shifted time logic

--- assets/js/planner.js
+++ assets/js/planner.js
@@ -1,44 +1,42 @@
 //// VARS
 var container = $(".container");
 let row = 0;
+var defaultRow =
+  '<div id="row" class="row">' +
+  '<div id="timeCol" class="col border border-2 border-secondary border-start-0"></div>' +
+  '<div id="taskCol" class="col-10 bg-secondary border border-light bg-opacity-25"></div>' +
+  '<div id="saveCol" class="col bg-info border border-light rounded-end rounded-3"></div>' +
+  "</div>";
 
 //// FUNCTIONS
 // to make the first row - testing to get function working properly
-var rowDefault = '<div id="row" class="row"></div>';
-var rowDefaultContent =
-  '<div id="timeCol" class="col border border-2 border-secondary border-start-0"></div> ' +
-  '<div id="taskCol" class="col-10 bg-secondary border border-light bg-opacity-25"></div>' +
-  '<div id="saveCol" class="col bg-info border border-light rounded-end rounded-3"></div>';
 
 // create container to hold each row (time, task, save)
 var createRow = function (rowNum) {
   container.append(rowNum);
-  console.log("start function createContainer");
 };
 
-var createRowContent = function (rowContent) {
-  $("#row").append(rowContent);
-  console.log("appends 3 containers to each row");
-};
-
-// change element's id to reflect their place
+// change element's id to reflect their time block
 var changeId = function (id, index) {
   $(id).attr("id", id + index);
+};
+
+// function to append time <p> to timeCol
+var appendTime = function (id, index) {
+  $(id + index);
+  $("#timeCol").append("<p>" + (index + 8) + "AM</p>");
 };
 
 // function to create 9 rows
 var createTimeRow = function () {
   while (row < 9) {
     row++;
-    createRow(rowDefault);
-    createRowContent(rowDefaultContent);
+    createRow(defaultRow);
     // make row id reflect place on list
     changeId("#row", row);
-    // add a time 9am-5pm to each timeCol
-    for (var num = 9; num <= 13; num++) {
-      $("#row" + row);
-      $("#timeCol").append("<p>" + (row + 8) + "AM</p>");
-    }
+    // at time within timeCol to reflect hours 9-5pm
+    appendTime("#row", row);
+    console.log("append next time");
   }
 };
 
